c3c3172ae59027957e6f3dfea66b574c3d0c311b
Yifei Gao <leng_ning@msn.com>
2023-05-05 18:59:14 +0800

    Updates

---
+++ src/images/project3-f81f99e6.js
@@ -0,0 +1,1 @@
+import"./plugins-86dce342.js";/* empty css              */const n=document.querySelector(".hud-4"),e=document.querySelector(".hud-5");setInterval(function(){const t=Math.round(Math.random()*100);n.textContent=t},1e3);setInterval(function(){const t=Math.round(Math.random()*100);e.textContent=t},1500);
